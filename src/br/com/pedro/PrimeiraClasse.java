970d325ba29c3b4c172e10a20ad3ecda9cd0fd72
Administrador <p.pecoo78@gmail.com>
2025-01-29 12:22:35 -0300

    teste

---
+++ src/br/com/pedro/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package br.com.pedro;
+
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        String ambiente = args[0];
+        if (ambiente.equals("Dev")) {
+            System.out.print(ambiente);
+        }
+        else {
+            System.out.print("Hello World, missing change ARGS environment");
+        }
+    }}
